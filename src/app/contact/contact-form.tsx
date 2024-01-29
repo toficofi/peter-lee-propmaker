b0a1cbdef3f76ea94b49412b0969375699b71d2f
Josh Lee <josh@joshbe.me>
2024-01-29 15:46:42 +0000

    Add Metadata import to contact form

--- src/app/contact/contact-form.tsx
+++ src/app/contact/contact-form.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { CheckCircle, CheckCircle2, Loader } from "lucide-react";
+import { Metadata } from "next";
 import { useState } from "react";
 
 export const metadata: Metadata = {
